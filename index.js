91e8621415986c00761dddbb6d14069c0734417a
Demet Çalışkan <caliskand@mef.edu.tr>
2023-05-29 01:58:14 +0300

    update new api

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function displayNews(articles) {
     const articleDiv = document.createElement('div')
     const title = document.createElement('a')
     title.innerHTML = article.title
-    title.href = article.url
+    title.href = article.link
     articleDiv.append(title)
     newsDiv.appendChild(articleDiv)
   }
@@ -13,8 +13,13 @@ function displayNews(articles) {
 
 async function logJSONData() {
   const url =
-    'https://newsapi.org/v2/top-headlines?country=tr&apiKey=92b381d24198400db19db155dc0e5945'
-  await fetch(url)
+    'https://api.newscatcherapi.com/v2/latest_headlines?countries=TR&lang=tr&when=24h'
+  await fetch(url, {
+    method: 'GET',
+    headers: {
+      'x-api-key': 'cnDKBtGz9fNseStWAhMGnQgC1dT7mahtZkkKAfJXt-4',
+    },
+  })
     .then((response) => response.json())
     .then((data) => displayNews(data.articles))
 }
